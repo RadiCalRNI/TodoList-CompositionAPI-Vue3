0afbd007d99fe0b09daec25c622499be62a0da42
Mohammad Rouhani <m.rouhani2001@gmail.com>
2023-02-14 13:50:25 +0330

    AppHeader component convert to composition API

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,25 +16,18 @@
   </header>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      defaultTheme: "dark",
-    };
-  },
-  methods: {
-    changeTheme() {
-      if (this.defaultTheme == "dark") {
-        this.defaultTheme = "light";
-        document.body.classList.add("light");
-      } else {
-        this.defaultTheme = "dark";
-        document.body.classList.remove("light");
-      }
-    },
-  },
-};
+<script setup>
+import { ref } from "vue";
+const defaultTheme = ref("dark");
+function changeTheme() {
+  if (defaultTheme.value == "dark") {
+    defaultTheme.value = "light";
+    document.body.classList.add("light");
+  } else {
+    defaultTheme.value = "dark";
+    document.body.classList.remove("light");
+  }
+}
 </script>
 
 <style></style>
